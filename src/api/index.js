b32f39c9bfdb75e227ca94c86a73cfd1f2cfe8e7
Frank Lan <franklan118@gmail.com>
2018-03-28 12:33:10 +1100

    optimise code

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,26 @@
+const vision = (filePath, callback) => {
+    const vision = require('./vision');
+    vision.get(filePath, labelAnnotations => {
+        labelAnnotations = vision.filter(labelAnnotations, process.env.VISON_SCORE_THRESHOLD);
+        labelAnnotations = vision.extract(labelAnnotations);
+        callback(labelAnnotations);
+    }, e => {
+        throw e
+    });
+};
+
+const translate = (q, target, callback) => {
+    const translate = require('./translation');
+
+    translate.get(q, target, translations => {
+        translations = translate.extract(translations);
+        callback(translations);
+    });
+};
+
 module.exports = app => {
     const multer = require('multer');
     const upload = multer({dest: '/tmp/'});
-    const vision = require('./vision');
-    const translate = require('./translation');
 
     app.get('/api/', (req, res) => {
         res.send(['Hello', 'API']);
@@ -11,30 +29,18 @@ module.exports = app => {
     app.post('/api/vision/label', upload.single('image'), (req, res) => {
         const filePath = req.file.path;
 
-        vision.get(filePath, labelAnnotations => {
-            labelAnnotations = vision.filter(labelAnnotations, process.env.VISON_SCORE_THRESHOLD);
-            labelAnnotations = vision.extract(labelAnnotations);
-            res.send(labelAnnotations);
-        }, e => {
-            const msg = process.env.mode === 'production' ? 'Unexpected error. Please contact your system administrator.' : `Google vision api error: ${e}`;
-            res.status(500).send(msg);
+        vision(filePath, labels => {
+            res.send(labels);
         });
     });
 
     app.post('/api/vision/label/translate', upload.single('image'), (req, res) => {
         const filePath = req.file.path;
 
-        vision.get(filePath, labelAnnotations => {
-            labelAnnotations = vision.filter(labelAnnotations, process.env.VISON_SCORE_THRESHOLD);
-            labelAnnotations = vision.extract(labelAnnotations);
-
-            translate.get(labelAnnotations, 'de', translations => {
-                translations = translate.extract(translations);
+        vision(filePath, labels => {
+            translate(labels, 'de', translations => {
                 res.send(translations);
             });
-        }, e => {
-            const msg = process.env.mode === 'production' ? 'Unexpected error. Please contact your system administrator.' : `Google vision api error: ${e}`;
-            res.status(500).send(msg);
         });
     });
 };
