449055b0fbd4dd8366b37937573f38ff3fe730cf
Frank Lan <franklan118@gmail.com>
2018-03-28 14:18:05 +1100

    tts

--- src/api/index.js
+++ src/api/index.js
@@ -18,6 +18,12 @@ const translate = (q, target, callback) => {
     });
 };
 
+const tts = (q, target, callback) => {
+    const tts = require('./testToSpeech');
+
+    tts.get(q, target, callback);
+};
+
 module.exports = app => {
     const multer = require('multer');
     const upload = multer({dest: '/tmp/'});
@@ -40,6 +46,22 @@ module.exports = app => {
         vision(filePath, labels => {
             translate(labels, 'de', translations => {
                 res.send(translations);
+            });
+        });
+    });
+
+    app.post('/api/vision/label/tts', upload.single('image'), (req, res) => {
+        const filePath = req.file.path;
+        let target = 'de'; //TODO: translate google language naming to watson naming
+
+        vision(filePath, labels => {
+            translate(labels, target, translations => {
+                console.log(translations[0]);
+                target = 'de-DE_BirgitVoice'; //TODO: translate google language naming to watson naming
+                tts(translations[0], target, speech => {
+                    res.header('Content-Type', 'audio/wav');
+                    res.send(speech);
+                });
             });
         });
     });
---
+++ src/api/testToSpeech.js
@@ -0,0 +1,24 @@
+const watson = require('watson-developer-cloud/text-to-speech/v1');
+const tts = new watson({
+    username: process.env.WATSON_TTS_USERNAME,
+    password: process.env.WATSON_TTS_PASSWORD,
+    url: 'https://stream.watsonplatform.net/text-to-speech/api/',
+});
+
+const get = (q, target, callback) => {
+    tts.synthesize(
+        {
+            text: q,
+            voice: target,
+            accept: 'audio/wav'
+        },
+        function(err, audio) {
+            tts.repairWavHeader(audio);
+            callback(audio)
+        }
+    );
+};
+
+module.exports = {
+    get,
+};
