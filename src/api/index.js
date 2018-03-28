7b276629ba2f369c181b75a16d16afb122a696a9
Frank Lan <franklan118@gmail.com>
2018-03-28 12:22:02 +1100

    translator

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ module.exports = app => {
     const multer = require('multer');
     const upload = multer({dest: '/tmp/'});
     const vision = require('./vision');
+    const translate = require('./translation');
 
     app.get('/api/', (req, res) => {
         res.send(['Hello', 'API']);
@@ -11,9 +12,26 @@ module.exports = app => {
         const filePath = req.file.path;
 
         vision.get(filePath, labelAnnotations => {
-            labelAnnotations = vision.filter(labelAnnotations, 0.96);
+            labelAnnotations = vision.filter(labelAnnotations, process.env.VISON_SCORE_THRESHOLD);
             labelAnnotations = vision.extract(labelAnnotations);
             res.send(labelAnnotations);
+        }, e => {
+            const msg = process.env.mode === 'production' ? 'Unexpected error. Please contact your system administrator.' : `Google vision api error: ${e}`;
+            res.status(500).send(msg);
+        });
+    });
+
+    app.post('/api/vision/label/translate', upload.single('image'), (req, res) => {
+        const filePath = req.file.path;
+
+        vision.get(filePath, labelAnnotations => {
+            labelAnnotations = vision.filter(labelAnnotations, process.env.VISON_SCORE_THRESHOLD);
+            labelAnnotations = vision.extract(labelAnnotations);
+
+            translate.get(labelAnnotations, 'de', translations => {
+                translations = translate.extract(translations);
+                res.send(translations);
+            });
         }, e => {
             const msg = process.env.mode === 'production' ? 'Unexpected error. Please contact your system administrator.' : `Google vision api error: ${e}`;
             res.status(500).send(msg);
---
+++ src/api/translation.js
@@ -0,0 +1,23 @@
+const get = (q, target, callback, options = {}) => {
+    const translator = require('google-translate')(process.env.GOOGLE_API_KEY);
+
+    if (q instanceof String && q.length) q = [q];
+
+    translator.translate(q, 'en', target, (err, translations) => {
+        console.log(translations);
+        if (callback instanceof Function) callback(translations);
+    });
+};
+
+const extract = (translations = []) => {
+    const extract = ({translatedText}) => {
+        return translatedText;
+    };
+
+    return translations.map(extract);
+};
+
+module.exports = {
+    get,
+    extract,
+};
