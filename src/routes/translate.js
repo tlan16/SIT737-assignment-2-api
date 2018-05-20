c3c3e65a2ec621bcb26e18610ef9f498950a5917
Frank Lan <franklan118@gmail.com>
2018-05-20 20:59:33 +1000

    separate out translate service

--- src/routes/translate.js
+++ src/routes/translate.js
@@ -1,33 +1,9 @@
-const Helpers = require('../helpers')
-
-const vision = (filePath, callback) => {
-  const vision = require('../services/vision')
-  vision.get(filePath, labelAnnotations => {
-    labelAnnotations = vision.filter(labelAnnotations, process.env.VISON_SCORE_THRESHOLD)
-    labelAnnotations = vision.extract(labelAnnotations)
-    callback(labelAnnotations)
-  }, e => {
-    throw e
-  })
-}
-
-const service = (q, target, callback) => {
-  const translate = require('../services/translation')
-
-  translate.get(q, target, translations => {
-    translations = translate.extract(translations)
-    callback(translations)
-  })
-}
+const service = require('../services/translation')
 
 const translate = app => {
-  app.post('/api/vision/label/translate', Helpers.getPostData().single('image'), (req, res) => {
-    const filePath = req.file.path
-
-    vision(filePath, labels => {
-      service(labels, 'de', translations => {
-        res.send(translations)
-      })
+  app.get('/api/translate/:text', (req, res) => {
+    service.get(req.params.text, req.query.language, ({translatedText}) => {
+      res.send(translatedText)
     })
   })
 }
