1e52540da7f5113b80b6455a0c8b291fd23a8437
Frank Lan <franklan118@gmail.com>
2018-05-23 18:05:51 +1000

    send octet stream if requested

--- src/routes/testToSpeech.js
+++ src/routes/testToSpeech.js
@@ -2,10 +2,14 @@ const {tts: Tts, voices: Voices} = require('../services/testToSpeech')
 
 const testToSpeech = app => {
   app.get('/api/text-to-speech/speak/:text', (req, res) => {
-    Tts(req.params.text, req.query.voice, speech =>
-      res.header('Content-Type', 'audio/wav')
+    Tts(req.params.text, req.query.voice, speech => {
+      if (req.headers['accept'] === 'application/octet-stream')
+        return res.header('Content-Type', 'application/octet-stream')
+          .send([...speech])
+
+      return res.header('Content-Type', 'audio/wav')
         .send(speech)
-    )
+    })
   })
 
   app.get('/api/text-to-speech/voices', (req, res) => {
